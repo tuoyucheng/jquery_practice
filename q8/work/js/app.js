3f0f09dc039ff2fe754805f65225cb96111bb61f
tuoyucheng <amazon60@icloud.com>
2023-06-16 12:48:23 +0900

    6/16

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,5 +1,5 @@
 $(function () {
-  function verb(firstDate) {
+  function infoShow(firstDate) {
     //エラーメッセージを消す。
     $(".message").remove();
     //空の変数creationを作る。
@@ -20,35 +20,48 @@ $(function () {
     }) : $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードを検索してください。</div>')
   }
   //pageOne（データの1ページ目の内容）を宣言。
-  let pageOne = 1,
-    quotation = "";
-  //検索ボタンを押した場合、
+  let pageOne = 1;
+  //quotation（""）を宣言。
+  let quotation = "";
+  //検索ボタンを押したとき、
   $(".search-btn").on("click", function () {
     //#search-inputにあるバリューをkeywordの中に入れる。
-    let keyword = $("#search-input").val();
-    //keywordがquotationと違う場合はpageOneに1を入れて、listsを空にしてほしい。quotationにkeywordを入れる。pageOneとquotationが同じ場合pageOne++が実行される。
-    keyword !== quotation ? (pageOne = 1, $(".lists").empty(), quotation = keyword) : pageOne++;
-
+    const keyword = $("#search-input").val();
+    //keywordがquotationと違う場合はpageOneに1を入れて、
+    if (keyword !== quotation) {
+      pageOne = 1;
+      //listsを空にする。
+      $(".lists").empty();
+      //quotationにkeywordを入れる。
+      quotation = keyword;
+      //pageOneとquotationが同じ場合は、pageOne++が実行される。
+    } else {
+      pageOne++;
+    }
+    //ajaxが実行されて、リンク先からデータを持ってくる。(keywordに入力した内容、pageOneは持ってきたデータの1ページ目の内容)
     $.ajax({
-      //ajaxが実行されて、リンク先からデータを持ってくる。(keywordに入力した内容、pageOneは持ってきたデータの1ページ目の内容)
       url: "https://ci.nii.ac.jp/books/opensearch/search?title=" + keyword + "&format=json&p=" + pageOne + "&count=20",
       method: "GET"
       //通信が成功した場合、doneが実行され、失敗したらfailが実行される。
     }).done(function (creation) {
       //成功したデータがcreationに入る。
-      verb(creation["@graph"])
+      infoShow(creation["@graph"])
       //creationのなかに失敗データが入っている。
-    }).fail(function (creation) {
+    }).fail(function (jqXHR) {
       //failの時はリストを空にする。
       $(".lists").empty();
       //メッセージを削除する。（この操作の前に既に失敗していることを想定している。そのエラーメッセージを消す。）
       $(".message").remove();
-      //creationのステータスは0か？0だったら下記メッセージを表示。（0、400はエラーステータスのこと）
-      0 === creation.status ? $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>') :
-        //creationのステータスが400だったら下記メッセージを表示。
-        400 === creation.status ? $(".lists").before('<div class="message">検索キーワードがありません。<br>文字以上で検索してください。</div>') :
+      //jqXHRのステータスは0か？0だったら下記メッセージを表示。（0、400はエラーステータスのこと）
+      if (jqXHR.status === 0) {
+        $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>');
+        //jqXHRのステータスが400だったら下記メッセージを表示。
+      } else if (jqXHR.status === 400) {
+        $(".lists").before('<div class="message">検索キーワードがありません。<br>文字以上で検索してください。</div>');
         //0でも400でもなかったら、下記メッセージを表示。
-        $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>')
+      } else {
+        $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>');
+      }
     })
   });
   //.reset-btnが押された場合。
